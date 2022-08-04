6fcbf39fa1b2d5028f10f979502aeb76bec327a0
gaurbprajapati <91283293+gaurbprajapati@users.noreply.github.com>
2022-08-04 17:18:40 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -70,3 +70,44 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [namech, setNamech] = useState("");
+
+//   function toggle(value) {
+//     // const s = event.target.value;
+//     // console.log(s);
+//     setName(event.target.value);
+//   }
+
+//   function toggleAdd() {
+//     setNamech(name);
+//   }
+//   return (
+//     <div className="container">
+//       <h1>Hello {namech} </h1>
+//       <input
+//         onChange={toggle}
+//         value={name}
+//         type="text"
+//         placeholder="What's your name?"
+//       />
+//       <button onClick={toggleAdd}>Submit</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
